cad87adf54774663a56fda25c4836e3b4c3b4c55
Deepanshu Gulia <deepanshugulia256@gmail.com>
2022-11-15 11:23:57 +0530

    added changes on skills page

--- src/components/skillsPageCom/SkillsComBanner.jsx
+++ src/components/skillsPageCom/SkillsComBanner.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import "./SkillsComBanner.css";
 
 export const SkillsComBanner = ({ data }) => {
-
   const { image, title, description } = data;
   const color = "green";
 
   return (
     <div
       className="skills-banner-main"
-      style={{ background: `url(${image})`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}
+      style={{
+        background: `url(${image})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
     >
       <div className="skills-banner-content">
         <h1>{title}</h1>
         <p>{description}</p>
       </div>
+
+      <button>Get Started</button>
     </div>
   );
 };
