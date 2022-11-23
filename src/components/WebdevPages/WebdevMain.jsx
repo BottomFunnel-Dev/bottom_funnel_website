e94c85225feb063bb1234a23c89210f967a360d6
hemantjayas <hemantjayas03@gmail.com>
2022-11-23 18:58:16 +0530

    changes

--- src/components/WebdevPages/WebdevMain.jsx
+++ src/components/WebdevPages/WebdevMain.jsx
@@ -6,13 +6,34 @@ import { webDevServices } from "../../Data/webdevPageData/Popularservices";
 import { GetStarted } from "./webGetStartedCard/GetStarted";
 import { WebDevWhy } from "./webDevWhy/WebDevWhy";
 import { mernWhyContent } from "../../Data/webdevPageData/whyBottomContent";
+import StoriesSection from "../StoriesSection/StoriesSection";
+import { Industries } from "../industries/Industries";
+import OndemandSection from "../OnDemandSection/OndemandSection";
+import PortFolio from "../PortFolioSection/PortFolio";
+import PlanProject from "../PlanProject/PlanProject";
+import Revamping from "../revamping/Revamping";
+import { SeoblogSection } from "../SeoBlogs/Seoblogs";
+import { TextDropdown } from "../faqSection/Faqs";
+import ContectForm from "../ContectForm/ContectForm";
+import MainFooter from "../FooterSection/MainFooter";
 export const WebdevMain = () => {
   return (
     <div>
       <WebdevBanner bannerContent={webDevMain} />
       <WebDevServices webservicesContent={webDevServices} />
-      <GetStarted/>
+      <GetStarted />
       <WebDevWhy whyDevContent={mernWhyContent} />
+
+      <StoriesSection />
+      <Industries />
+      <PlanProject />
+      <OndemandSection />
+      <PortFolio />
+      <SeoblogSection />
+      <Revamping />
+      <TextDropdown />
+      <ContectForm />
+      <MainFooter />
     </div>
   );
 };
