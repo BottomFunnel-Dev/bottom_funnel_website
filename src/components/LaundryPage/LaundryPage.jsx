7010f5fe3ac299f05a99b034792385ec621bd261
hemantjayas <hemantjayas03@gmail.com>
2022-11-30 19:26:44 +0530

    changes

--- src/components/LaundryPage/LaundryPage.jsx
+++ src/components/LaundryPage/LaundryPage.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import ContectForm from '../ContectForm/ContectForm'
+import { TextDropdown } from '../faqSection/Faqs'
+import MainFooter from '../FooterSection/MainFooter'
+import { Industries } from '../industries/Industries'
+import OndemandSection from '../OnDemandSection/OndemandSection'
+import PlanProject from '../PlanProject/PlanProject'
+import PortFolio from '../PortFolioSection/PortFolio'
+import Revamping from '../revamping/Revamping'
+import { SeoblogSection } from '../SeoBlogs/Seoblogs'
+import StoriesSection from '../StoriesSection/StoriesSection'
 import LaudryApplication from './LaudryApplication'
 import LaundryAdmin from './LaundryAdmin'
 import Laundryapppagelowerfooter from './Laundryapppagelowerfooter'
@@ -18,6 +28,17 @@ export const LaundryPage = () => {
       <LaundryAdmin />
       <LaundryBannerlower />
       <Laundryapppagelowerfooter />
+
+      <StoriesSection />
+      <Industries />
+      <PlanProject />
+      <OndemandSection />
+      <PortFolio />
+      <SeoblogSection />
+      <Revamping />
+      <TextDropdown />
+      <ContectForm />
+      <MainFooter />
 
     </div>
   )
--- src/components/MarketPlace/MarketPlace.jsx
+++ src/components/MarketPlace/MarketPlace.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import Maketplacefooter from './Maketplacefooter'
-import MarketPlaceAdmin from './MarketPlaceAdmin'
-import { MarketplaceBanner } from './MarketplaceBanner'
-import Marketplaceborder from './Marketplaceborder'
-import MarketPlaceClients from './MarketPlaceClients'
+import React from "react";
+import ContectForm from "../ContectForm/ContectForm";
+import { TextDropdown } from "../faqSection/Faqs";
+import MainFooter from "../FooterSection/MainFooter";
+import { Industries } from "../industries/Industries";
+import OndemandSection from "../OnDemandSection/OndemandSection";
+import PlanProject from "../PlanProject/PlanProject";
+import PortFolio from "../PortFolioSection/PortFolio";
+import Revamping from "../revamping/Revamping";
+import { SeoblogSection } from "../SeoBlogs/Seoblogs";
+import StoriesSection from "../StoriesSection/StoriesSection";
+import Maketplacefooter from "./Maketplacefooter";
+import MarketPlaceAdmin from "./MarketPlaceAdmin";
+import { MarketplaceBanner } from "./MarketplaceBanner";
+import Marketplaceborder from "./Marketplaceborder";
+import MarketPlaceClients from "./MarketPlaceClients";
 
-import MarketplaceFreelancer from './MarketplaceFreelancer'
+import MarketplaceFreelancer from "./MarketplaceFreelancer";
 
 export const Marketplace = () => {
   return (
     <div>
-    <MarketplaceBanner />
-  <MarketplaceFreelancer />
-<MarketPlaceAdmin />
-  <MarketPlaceClients />
-  <Marketplaceborder />
-  <Maketplacefooter />
+      <MarketplaceBanner />
+      <MarketplaceFreelancer />
+      <MarketPlaceAdmin />
+      <MarketPlaceClients />
+      <Marketplaceborder />
+      <Maketplacefooter />
+
+      <StoriesSection />
+      <Industries />
+      <PlanProject />
+      <OndemandSection />
+      <PortFolio />
+      <SeoblogSection />
+      <Revamping />
+      <TextDropdown />
+      <ContectForm />
+      <MainFooter />
     </div>
-  )
-}
-
-
-
+  );
+};
