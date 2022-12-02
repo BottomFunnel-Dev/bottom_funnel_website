47cce680c69e1a475a37598033a25b8f32199de6
Deepanshu Gulia <deepanshugulia256@gmail.com>
2022-12-02 12:20:32 +0530

    done with navbar color issue

--- src/components/FlowerDelevry/FlowerDelevry.jsx
+++ src/components/FlowerDelevry/FlowerDelevry.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ContectForm from "../ContectForm/ContectForm";
 import { TextDropdown } from "../faqSection/Faqs";
 import MainFooter from "../FooterSection/MainFooter";
