42c58040f13d55e8858104bc056a93b67c05bb60
hemantjayas <hemantjayas03@gmail.com>
2022-12-14 08:19:55 +0530

    conflict removed

--- src/components/XamarinAppDevelopment/XamarinSolutions/XamarinSolutions.jsx
+++ src/components/XamarinAppDevelopment/XamarinSolutions/XamarinSolutions.jsx
@@ -2,25 +2,47 @@ import React from "react";
 import "./XamarinSolutions.css";
 
 export const XamarinSolutions = () => {
-
   return (
-      <>
-        <div className="xamarinSolutionsMainDiv">
-          <div className="xamarinSolutionsHeading">
-            <h1>Our Xamarin Cross-Platform App Development Solutions</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, voluptatib
-                us explicabo sequi optio possimus non nemo aliquam, iure laborum mollitia aperiam neque vero temporibus dolore sapiente doloremque consequuntur. Fuga, ea!</p>
-          </div>
-            <img className="xamarinSolutionsImage" src="/Images/Xamarin/Group 340011.png"/>
-                <img className="xamarinSolutions1" src='Images/Xamarin/icons/solution 3.png'/>
-                <h6 className="xamarinSolutions1text">Custom Xamarin App Development</h6>
-                <img className="xamarinSolutions3" src='Images/Xamarin/icons/solution 4.png'/>
-                <h6 className="xamarinSolutions3text">Xamarin UI/UX Development</h6>
-                <img className="xamarinSolutions2" src='Images/Xamarin/icons/solution 6.png'/>
-                <h6 className="xamarinSolutions2text">Prototype & wireframe Development</h6>
-                <img className="xamarinSolutions4" src='Images/Xamarin/icons/solution 5.png'/>
-                <h6 className="xamarinSolutions4text">Xamarin Migration Solutions</h6>
+    <>
+      <div className="xamarinSolutionsMainDiv">
+        <div className="xamarinSolutionsHeading">
+          <h1>Our Xamarin Cross-Platform App Development Solutions</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
+            voluptatib us explicabo sequi optio possimus non nemo aliquam, iure
+            laborum mollitia aperiam neque vero temporibus dolore sapiente
+            doloremque consequuntur. Fuga, ea!
+          </p>
         </div>
-      </>
+        <img
+          className="xamarinSolutionsImage"
+          src="/Images/Xamarin/Group 340011.png"
+        />
+        {/* <img
+          className="xamarinSolutions1"
+          src="Images/Xamarin/icons/solution 3.png"
+        />
+        <h6 className="xamarinSolutions1text">
+          Custom Xamarin App Development
+        </h6>
+        <img
+          className="xamarinSolutions3"
+          src="Images/Xamarin/icons/solution 4.png"
+        />
+        <h6 className="xamarinSolutions3text">Xamarin UI/UX Development</h6>
+        <img
+          className="xamarinSolutions2"
+          src="Images/Xamarin/icons/solution 6.png"
+        />
+        <h6 className="xamarinSolutions2text">
+          Prototype & wireframe Development
+        </h6>
+        <img
+          className="xamarinSolutions4"
+          src="Images/Xamarin/icons/solution 5.png"
+        />
+        <h6 className="xamarinSolutions4text">Xamarin Migration Solutions</h6> */}
+      </div>
+    </>
   );
 };
