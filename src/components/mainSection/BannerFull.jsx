6a9c4f3e28fddac61c60154fb9df5af9a479234f
hemantjayas <hemantjayas03@gmail.com>
2022-12-20 15:32:09 +0530

    changes

--- src/components/mainSection/BannerFull.jsx
+++ src/components/mainSection/BannerFull.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./bannerFull.css";
 import Slider from "react-slick";
 import { MobileBanner } from "./BannerAnimations/MobileBanner";
@@ -6,13 +6,11 @@ import { SoftwareBanner } from "./BannerAnimations/SoftwareBanner";
 import { DigitalMarketing } from "./BannerAnimations/DigitalMarketing";
 
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { PopupForm } from "../PopupForm/PopupForm";
 
 export const BannerFull = () => {
-
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -31,7 +29,6 @@ export const BannerFull = () => {
     p: 1,
     borderRadius: 2,
   };
-
 
   const banner = [
     {
@@ -121,7 +118,6 @@ export const BannerFull = () => {
                 <SoftwareBanner />
               )}
 
-                
               {item.title === "Digital Marketing" && <DigitalMarketing />}
 
               <div className="banner-text-content-full">
@@ -130,24 +126,23 @@ export const BannerFull = () => {
                 <button onClick={handleOpen}>Get Started</button>
 
                 <Modal
-                      open={open}
-                      onClose={handleClose}
-                      aria-labelledby="modal-modal-title"
-                      aria-describedby="modal-modal-description"
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Box sx={style}>
+                    <Typography
+                      id="modal-modal-title"
+                      variant="h6"
+                      component="h2"
                     >
-                      <Box sx={style}>
-                        <Typography
-                          id="modal-modal-title"
-                          variant="h6"
-                          component="h2"
-                        >
-                          <div className="popform">
-                            <PopupForm />
-                          </div>
-                        </Typography>
-                      </Box>
-                    </Modal>
-
+                      <div className="popform">
+                        <PopupForm />
+                      </div>
+                    </Typography>
+                  </Box>
+                </Modal>
               </div>
             </div>
           </div>
