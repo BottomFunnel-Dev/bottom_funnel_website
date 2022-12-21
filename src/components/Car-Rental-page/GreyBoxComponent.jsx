7899a947217de68848a009590951641aa6f86808
Travel-Flap <info@travelflap.com>
2022-12-21 15:03:54 +0530

    code changes 21-12-2022

--- src/components/Car-Rental-page/GreyBoxComponent.jsx
+++ src/components/Car-Rental-page/GreyBoxComponent.jsx
@@ -6,7 +6,7 @@ export const GreyBoxComponent = () => {
     <div className='RGgreyBoxContainer'>
         <div className='RGdivv'>
             <h2>Simple Login</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi sint soluta, cum illo veniam debitis porro libero labore inventore eos quae deserunt architecto similique hic esse quia dicta! Dolor, nemo?</p>
+            <p>Users ACn Create account and  log in with ease through email or social media platforms.</p>
         </div>
 
         <div className='RGdivv'>
@@ -21,7 +21,7 @@ export const GreyBoxComponent = () => {
         </div>
         <div className='RGdivv'>
            <h2> Reservation Management</h2>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi sint soluta, cum illo veniam debitis porro libero labore inventore eos quae deserunt architecto similique hic esse quia dicta! Dolor, nemo?</p>
+           <p>The reservation Panel  is accessible to the user and  has carrental</p>
 
         </div>
         <div className='RGdivv'>
