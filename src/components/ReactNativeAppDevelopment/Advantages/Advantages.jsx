ebdfd2fc1bcbe33433b867e2c913b59edc11c933
Aaradhana29 <kumariaaradhana99@gmail.com>
2023-01-05 12:23:32 +0530

    Added content in React Native page

--- src/components/ReactNativeAppDevelopment/Advantages/Advantages.jsx
+++ src/components/ReactNativeAppDevelopment/Advantages/Advantages.jsx
@@ -15,28 +15,22 @@ export const Advantages = () => {
                  <div className='rndivbox'>
                     <img src='/Images/ReactNativeDevelopement/lightbulb.png' alt='logo'  className='rnimag'></img>
                     </div>
-                    <h4>Watch Your App Idea Growing </h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in eaque quos reiciendis obcaecati
-                         modi culpa atque, quis neque deserunt nobis dolorum aperiam enim numquam. 
-                        Expedita necessitatibus veritatis fuga optio?</p>
+                    <h4>Single Code base and reusable code</h4>
+                    <p>React has a single code base and reusable code, which makes it easy for developers to use and maintain. This helps to keep the codebase clean and consistent, and also reduces the amount of time and effort required to develop and deploy applications.</p>
                  </div>
                  <div className='reactnativeinnermaindiv4-row'>
                  <div className='rndivbox'>
                     <img src='/Images/ReactNativeDevelopement/lightbulb-1.png' alt='logo'  className='rnimag'></img>
                     </div>
-                    <h4>Watch Your App Idea Growing </h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in eaque quos reiciendis obcaecati
-                         modi culpa atque, quis neque deserunt nobis dolorum aperiam enim numquam. 
-                        Expedita necessitatibus veritatis fuga optio?</p>
+                    <h4>Pre built Component</h4>
+                    <p>React has pre built components that help you build user interfaces quickly. With pre built components, you can simply drag and drop them into your project to get started quickly. This saves you time and effort in having to build your own components from scratch.</p>
                  </div>
                  <div className='reactnativeinnermaindiv4-row'>
                  <div className='rndivbox'>
                     <img src='/Images/ReactNativeDevelopement/lightbulb-2.png' alt='logo' className='rnimag'></img>
                     </div>
-                    <h4>Watch Your App Idea Growing </h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in eaque quos reiciendis obcaecati
-                         modi culpa atque, quis neque deserunt nobis dolorum aperiam enim numquam. 
-                        Expedita necessitatibus veritatis fuga optio?</p>
+                    <h4>Simple UI</h4>
+                    <p>React has a simple UI that makes it easy to use. The UI is designed to be intuitive and easy to use. There are no complicated menus or options to choose from. Everything is designed to be simple and straightforward. This makes React a great choice for those who want an easy to use UI.</p>
                  </div>
                 </div>
                 <div className='nativeinnermaindiv4-boxdiv1stcolum' >
@@ -44,28 +38,22 @@ export const Advantages = () => {
                  <div className='rndivbox'>
                     <img src='/Images/ReactNativeDevelopement/lightbulb-7.png' alt='logo'  className='rnimag'></img>
                     </div>
-                    <h4>Watch Your App Idea Growing </h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in eaque quos reiciendis obcaecati
-                         modi culpa atque, quis neque deserunt nobis dolorum aperiam enim numquam. 
-                        Expedita necessitatibus veritatis fuga optio?</p>
+                    <h4>Documentation </h4>
+                    <p>The React documentation is excellent! It's very well organized and written, and it covers all the topics you need to know about React in great detail. The examples are also very helpful. I highly recommend reading through the docs if you're planning on using React for your next project.</p>
                  </div>
                  <div className='reactnativeinnermaindiv4-row'>
                  <div className='rndivbox'>
                     <img src='/Images/ReactNativeDevelopement/lightbulb-8.png' alt='logo'  className='rnimag'></img>
                     </div>
-                    <h4>Watch Your App Idea Growing </h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in eaque quos reiciendis obcaecati
-                         modi culpa atque, quis neque deserunt nobis dolorum aperiam enim numquam. 
-                        Expedita necessitatibus veritatis fuga optio?</p>
+                    <h4>Modular Architecture</h4>
+                    <p>React has a modular architecture, which means that its code is divided into small modules. This makes it easy to update and maintain React applications. Additionally, the modular architecture makes it easier to reuse React components in other applications.</p>
                  </div>
                  <div className='reactnativeinnermaindiv4-row'>
                  <div className='rndivbox'>
                     <img src='/Images/ReactNativeDevelopement/lightbulb-3.png' alt='logo'  className='rnimag' ></img>
                     </div>
-                    <h4>Watch Your App Idea Growing </h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in eaque quos reiciendis obcaecati
-                         modi culpa atque, quis neque deserunt nobis dolorum aperiam enim numquam. 
-                        Expedita necessitatibus veritatis fuga optio?</p>
+                    <h4>Declarative Coding Style</h4>
+                    <p>React has a declarative coding style, which means that code is written in a way that is easy to read and understand. This makes it easy for developers to create complex applications with React. In addition, the declarative style also makes it easier to debug applications, as errors are easy to find and fix.</p>
                  </div>
                 </div>
 
@@ -76,28 +64,22 @@ export const Advantages = () => {
                  <div className='rndivbox'>
                     <img src='/Images/ReactNativeDevelopement/lightbulb-6.png' alt='logo'  className='rnimag'></img>
                     </div>
-                    <h4>Watch Your App Idea Growing </h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in eaque quos reiciendis obcaecati
-                         modi culpa atque, quis neque deserunt nobis dolorum aperiam enim numquam. 
-                        Expedita necessitatibus veritatis fuga optio?</p>
+                    <h4>Third Party Plugin Support</h4>
+                    <p>React is a JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.</p>
                  </div>
                  <div className='reactnativeinnermaindiv4-row'>
                  <div className='rndivbox'>
                     <img src='/Images/ReactNativeDevelopement/lightbulb-5.png' alt='logo'  className='rnimag'></img>
                     </div>
-                    <h4>Watch Your App Idea Growing </h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in eaque quos reiciendis obcaecati
-                         modi culpa atque, quis neque deserunt nobis dolorum aperiam enim numquam. 
-                        Expedita necessitatibus veritatis fuga optio?</p>
+                    <h4>Live and Hot Reloading</h4>
+                    <p>React has Live and Hot Reloading capabilities that make it easy to develop web applications. With these features, you can make changes to your code and see the results immediately in the browser. This makes React a great choice for developing modern web applications.</p>
                  </div>
                  <div className='reactnativeinnermaindiv4-row'>
                     <div className='rndivbox'>
                     <img src='/Images/ReactNativeDevelopement/lightbulb-4.png' alt='logo'  className='rnimag'></img>
                     </div>
-                    <h4>Watch Your App Idea Growing </h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in eaque quos reiciendis obcaecati
-                         modi culpa atque, quis neque deserunt nobis dolorum aperiam enim numquam. 
-                        Expedita necessitatibus veritatis fuga optio?</p>
+                    <h4>Shorter learning Curve</h4>
+                    <p>React has a shorter learning curve than other popular frameworks, making it a great choice for javascript developers who want to get started with building web applications quickly. The declarative syntax and use of components make React code easy to read and understand, even for those who are new to the framework. With a little practice, you'll be able to build React apps that are fast, responsive, and look great.</p>
                  </div>
                 </div>
 
@@ -118,7 +100,7 @@ export const Advantages = () => {
                 <div className='rnlogo'>
                     <img src='/Images/ReactNativeDevelopement/platform 2.png' alt='logo' className='rnlogoimg'></img>
                     </div>
-                 <div><h3>Rigorous Requirement Analysis</h3>
+                 <div><h3>Cross-platform App Development</h3>
                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus laboriosam cupiditate 
                     </p>
                  </div>
@@ -127,7 +109,7 @@ export const Advantages = () => {
                  <div className='rnlogo'>
                     <img src='/Images/ReactNativeDevelopement/approved 2.png' alt='logo' className='rnlogoimg'></img>
                     </div>
-                 <div><h3>Rigorous Requirement Analysis</h3>
+                 <div><h3>Hire Developers</h3>
                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus laboriosam cupiditate 
                     </p>
                  </div>
@@ -138,7 +120,7 @@ export const Advantages = () => {
                  <div className='rnlogo'>
                     <img src='/Images/ReactNativeDevelopement/app-development (2) 2.png' alt='logo' className='rnlogoimg'></img>
                     </div>
-                 <div><h3>Rigorous Requirement Analysis</h3>
+                 <div><h3>Industry Specific Solutions</h3>
                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus laboriosam cupiditate 
                     </p>
                  </div>
@@ -147,7 +129,7 @@ export const Advantages = () => {
                  <div className='rnlogo'>
                     <img src='/Images/ReactNativeDevelopement/technical-support 2.png' alt='logo' className='rnlogoimg'></img>
                     </div>
-                 <div><h3>Rigorous Requirement Analysis</h3>
+                 <div><h3>End-to-End Support and Maintaince</h3>
                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus laboriosam cupiditate 
                    </p>
                  </div>
